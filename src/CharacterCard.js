825f3894405845c18d29bdc30161489985ab2d8a
SingKup <thanawat452@gmail.com>
2020-09-11 11:17:28 +0700

    Check2.1+3

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,6 +1,21 @@
-import React from 'react';
+// import React from 'react';
+// import './App.css';
+// export default function CharacterCard(props) {
+//  return (
+//  <div className = "card">{props.value}</div>
+//  )
+// }
+
+import './App.css';
+import React, { useState } from 'react';
 export default function CharacterCard(props) {
+ const [active, setActive] = useState(false);
+ const activate = () => {
+ setActive(true)
+ }
+ const className = `card ${active ? 'activeCard': ''}`
  return (
- <div>{props.value}</div>
+ <div className={className} onClick={activate}>{props.value}</div>
  )
+
 }
