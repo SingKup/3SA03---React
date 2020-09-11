528cbb7c6331901a452064469548411fcdce5421
SingKup <thanawat452@gmail.com>
2020-09-11 10:27:08 +0700

    check2

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
+import CharacterCard from './CharacterCard';
+const word = "Hello";
 function App() {
-  return (
-    <div>
-      Hello World {1+2+3}
-      </div>
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+ return (
+ <div>
+ {
+ Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+ }
+ </div>
+ );
 }
+
 
 export default App;
